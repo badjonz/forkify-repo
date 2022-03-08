13369001774d4d6e41e054d621c3ab5d76712c55
badjonz <badjonz@gmail.com>
2022-03-08 20:57:40 +0900

    Added new welcome features

--- src/js/controller.js
+++ src/js/controller.js
@@ -119,6 +119,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeatures = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBoomarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -127,6 +131,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeatures();
 };
 init();
